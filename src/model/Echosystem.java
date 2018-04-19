087450cd1ace34073b2e5a56c99a457a129ba0ee
Juan Buendia <juan.buendia@uptc.edu.co>
2018-04-19 16:43:04 -0500

    Se creo la clase Echosystem

---
+++ src/model/Echosystem.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Echosystem {
+
+	private ArrayList<Specie> listSpecies;
+	
+	public Echosystem() {
+		listSpecies = new ArrayList<>();
+	}
+
+	public static Specie createSpecie(int id, String name, int lifeCicle, int adaptability, int groundKnowledge) {
+		return new Specie(id, name, lifeCicle, adaptability, groundKnowledge);
+	}
+	
+	public void addSpecie(Specie specie) {
+		listSpecies.add(specie);
+	}
+	
+	public ArrayList<Specie> getListSpecies() {
+		return listSpecies;
+	}
+}
