9df34793834263bba86c634a2f10d8ee37770726
Juan Buendia <juan.buendia@uptc.edu.co>
2018-04-19 16:35:05 -0500

    Se creo la clase Specie, con estructuras de datos propias de Java

---
+++ src/model/Specie.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Specie {
+
+	private int id;
+	private String name;
+	private int lifeCicle;
+	private int adaptability;
+	private int groundKnowledge;
+	private Queue<Animal> pregnancyQueue;
+	private LinkedList<Animal> animalGroup;
+	
+	public Specie(int id, String name, int lifeCicle, int adaptability, int groundKnowledge) {
+		this.id = id;
+		this.name = name;
+		this.lifeCicle = lifeCicle;
+		this.adaptability = adaptability;
+		this.groundKnowledge = groundKnowledge;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setLifeCicle(int lifeCicle) {
+		this.lifeCicle = lifeCicle;
+	}
+
+	public void setAdaptability(int adaptability) {
+		this.adaptability = adaptability;
+	}
+
+	public void setGroundKnowledge(int groundKnowledge) {
+		this.groundKnowledge = groundKnowledge;
+	}
+
+	public int getLifeCicle() {
+		return lifeCicle;
+	}
+
+	public int getAdaptability() {
+		return adaptability;
+	}
+
+	public int getGroundKnowledge() {
+		return groundKnowledge;
+	}
+
+	public Queue<Animal> getPregnancyQueue() {
+		return pregnancyQueue;
+	}
+
+	public LinkedList<Animal> getAnimalGroup() {
+		return animalGroup;
+	}
+
+	@Override
+	public String toString() {
+		return "Specie [id=" + id + ", name=" + name + ", lifeCicle=" + lifeCicle + ", adaptability=" + adaptability
+				+ ", groundKnowledge=" + groundKnowledge + "]";
+	}
+}
